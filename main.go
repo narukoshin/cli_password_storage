6429cf8a55b34c67a1a0bd21e3c719efe2bafb9c
Naru Koshin <naru.koshin@outlook.jp>
2021-12-08 10:36:10 +0200

    added some new ideas about the project

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// todo:
+	// There will be some actions for example - creating a new folder where the passwords will be stored.
+	// If the user wants, he can set a custom password to access the folders.
+	// Each folder can be protected with the different password.
+
+}
